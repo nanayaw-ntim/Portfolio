1a722b6285b19897df9c89fdd8c40eb84b70a137
Katsnite <nanantim.yaw@gmail.com>
2021-11-14 09:33:17 +0000

    Update sorting.py
    Creating the Menu and return request as well as how users will make the array

---
+++ src/sorting.py
@@ -0,0 +1,53 @@
+def BubbleSort(Array):
+	unsortedArray = Array
+
+
+
+# Allows user to select a sorting mechanism
+def Menu(Array):
+	print("Sorting Mechanisms Available\n"
+		  "1. Bubble Sort\n"
+		  "2. Insertion Sort\n\n"
+		  "Select an option with the number in front of it")
+
+	choice = int(input("> "))
+
+	if choice == 1:
+		BubbleSort(Array)
+	elif choice == 2:
+		print("Coming Soon...")
+	else:
+		print("Choice not available")
+		Return(Array)
+
+
+# When a user wants to go back to the Menu
+def Return(Array):
+	request = input("Return to Menu?(y/n)")
+
+	if request.Upper() == "Y":
+		Menu(Array)
+	elif request.Upper() == "N":
+		print("Good bye!")
+
+
+if __name__ ==  "__main__":
+	userArray = []
+	arrayLen = int(input("Number of elements in array : "))
+	Counter = arrayLen
+
+	# Allows user to fill array with data
+	for index in range(arrayLen):
+		num = int(input("Num : "))
+		Counter = Counter - 1
+		
+		if Counter > 1:
+			print(f"{Counter} numbers left...")
+		elif Counter == 1:
+			print(f"{Counter} number left...")
+
+		userArray.append(num)
+
+	# Initializes the menu
+	Menu(userArray)
+
